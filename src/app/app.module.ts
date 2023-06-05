6abf67ad740001b745a52b9c48d3d5d0ee639bc9
Cong Le <congldqn888@gmail.com>
2023-06-06 05:36:04 +0700

    feature: design the admin page layout interface refs: AG9W-14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,33 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { WebsiteLayoutComponent } from './layouts/website-layout/website-layout.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { NavigationComponent } from './components/admin/navigation/navigation.component';
+import { SideBarComponent } from './components/admin/side-bar/side-bar.component';
+import { HeaderComponent } from './components/client/header/header.component';
+import { FooterComponent } from './components/client/footer/footer.component';
+import { HomePageComponent } from './pages/client/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/client/products-page/products-page.component';
+import { ProductDetailPageComponent } from './pages/client/product-detail-page/product-detail-page.component';
+import { RegisterPageComponent } from './pages/client/register-page/register-page.component';
+import { LoginPageComponent } from './pages/client/login-page/login-page.component';
+import { DashBoardPageComponent } from './pages/admin/dash-board-page/dash-board-page.component';
 
 @NgModule({
-  declarations: [AppComponent, WebsiteLayoutComponent, AdminLayoutComponent],
+  declarations: [
+    AppComponent,
+    WebsiteLayoutComponent,
+    AdminLayoutComponent,
+    NavigationComponent,
+    SideBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProductsPageComponent,
+    ProductDetailPageComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    DashBoardPageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
